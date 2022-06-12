30629a6902de30417073eb96d3af4458ac4ce115
58784 <k56k.k-0504@docomo.ne.jp>
2022-06-12 15:04:32 +0900

    Fix: 25分カウントダウン機能

--- html/js/popup.js
+++ html/js/popup.js
@@ -14,7 +14,7 @@ makeTimerBtn.onclick = function () {
     let work_time = document.forms.timerForm.work_time.value;
     let interval = document.forms.timerForm.interval.value;
 
-    document.getElementById("work_time").innerHTML = `${work_time}:00`;
+    document.getElementById("work_time").innerHTML = String(work_time).padStart(2,"0") + ":00";
     if (repeat_time > 0) {
         document.getElementById("timerDetails").innerHTML = `${repeat_time}時間の間<br>${work_time}分集中 / ${interval}分休憩を繰り返す`;
     }else{
@@ -29,26 +29,48 @@ makeTimerBtn.onclick = function () {
     document.getElementById("noTimer").style.display ="none";
 };
 
-//スタートボタンを押した時
-startBtn.addEventListener('click', function() {
-    setInterval(count_down, 1000);
+startBtn.onclick = function() {
+    start();
+};
 
-    let work_time = parseInt(document.forms.timerForm.work_time.value, 10);
-    let time = work_time * 60
-    let min = 0;
-    let sec = 0;
+stopBtn.onclick = function() {
+    stop();
+};
 
-    let timer = document.getElementById("work_time");
-    timer.innerHTML = `${work_time}:00`;
+resetBtn.onclick = function() {
+    reset();
+};
 
-    function count_down() {
-        if (time > 0) {
-            let min = Math.floor(time / 60);
-            let sec = time % 60;   
-            time--;
-            timer.textContent = String(min).padStart(2,"0") + ":" + String(sec).padStart(2,"0");
-        }else if (time === 0) {
-            timer.innerHTML = "TIME UP!";
-        }
+// タイマー停止用ID
+let intervalId;
+let work_time = parseInt(document.forms.timerForm.work_time.value, 10);
+let time = work_time * 60
+let work_timer = document.getElementById("work_time");
+
+function start() {
+    if (intervalId == null) {
+        intervalId = setInterval(count_down, 1000);
     }
-});
+}
+
+function stop() {
+    clearInterval(intervalId);
+    intervalId = null;
+};
+
+function reset() {
+    // 経過秒数を初期化
+    // spanedSec = work_time;
+    // work_timer.innerHTML = String(spanedSec).padStart(2,"0") + ":00";
+};
+
+function count_down() {
+    if (time > 0) {
+        let min = Math.floor(time / 60);
+        let sec = time % 60;   
+        time--;
+        work_timer.innerHTML = String(min).padStart(2,"0") + ":" + String(sec).padStart(2,"0");
+    }else if (time === 0) {
+        work_timer.innerHTML = "TIME UP!";//後でここに休憩時間入れるかも？
+    }
+};
