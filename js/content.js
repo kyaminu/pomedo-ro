d1fae5d338172c67a9d3e452a31e3ed16dde0638
kamin_k <m7mkmnn@gmail.com>
2023-02-20 17:46:32 +0900

    doalogの位置を固定

--- js/content.js
+++ js/content.js
@@ -12,17 +12,14 @@ const pome_work_alarm_url = chrome.runtime.getURL('src/mp3/pome_work_alarm.mp3#t
 const pome_interval_alarm_url = chrome.runtime.getURL('src/mp3/pome_interval_alarm.mp3');
 const finish_alarm_url = chrome.runtime.getURL('src/mp3/finish_alarm.mp3');
 
-//キャラのcss
-const pome_style = "width: 100px; height: auto; bottom: 0px; left: 0px; position: fixed; z-index: 9999999;"
-
 //HTML画像タグ
-const pome_hppy_tag = `<img src="${pome_happy_url}" id="pome_happy_img" style="${pome_style}">`;
-const pome_angry_tag = `<img src="${pome_angry_url}" id="pome_angry_img" style="${pome_style}">`;
+const pome_hppy_tag = `<img src="${pome_happy_url}" id="pome_happy_img" >`;
+const pome_angry_tag = `<img src="${pome_angry_url}" id="pome_angry_img">`;
 
-//キャラをcssに挿入（初期は非表示）
-document.querySelector("head").insertAdjacentHTML("afterbegin", '<style>#pome_happy_img {width: 100px; height: auto; bottom: 0px; left: 0px; position: fixed; z-index: 9999999; animation: pome_happy_move 5s linear 1s 12;} @keyframes pome_happy_move {0%   { transform: scale(1.0, 1.0) translate(0%, 0%); }10%  { transform: scale(1.1, 0.9) translate(0%, 5%); } 40%  { transform: scale(1.2, 0.8) translate(0%, 15%); } 50%  { transform: scale(1.0, 1.0) translate(0%, 0%); } 60%  { transform: scale(0.9, 1.2) translate(0%, -100%); } 75%  { transform: scale(0.9, 1.2) translate(0%, -20%); } 85%  { transform: scale(1.2, 0.8) translate(0%, 15%); } 100% { transform: scale(1.0, 1.0) translate(0%, 0%); }}}</style>');
+//キャラのcssを挿入
+document.querySelector("head").insertAdjacentHTML("afterbegin", '<style>#pome_happy_img {width: 100px; height: auto; bottom: 0px; left: 0px; position: fixed; z-index: 9999999; border-style: none; animation: pome_happy_move 5s linear 1s 12;} @keyframes pome_happy_move {0%   { transform: scale(1.0, 1.0) translate(0%, 0%); }10%  { transform: scale(1.1, 0.9) translate(0%, 5%); } 40%  { transform: scale(1.2, 0.8) translate(0%, 15%); } 50%  { transform: scale(1.0, 1.0) translate(0%, 0%); } 60%  { transform: scale(0.9, 1.2) translate(0%, -100%); } 75%  { transform: scale(0.9, 1.2) translate(0%, -20%); } 85%  { transform: scale(1.2, 0.8) translate(0%, 15%); } 100% { transform: scale(1.0, 1.0) translate(0%, 0%); }}}</style>');
 
-document.querySelector("head").insertAdjacentHTML("afterbegin", '<style>#pome_angry_img {width: 100px; height: auto; bottom: 0px; left: 0px; position: fixed; z-index: 9999999; animation: pome_angry_move 10s linear 1s 12;} @keyframes pome_angry_move {0% { transform: scale(1.0, 1.0) translate(0%, 0%); }15%  { transform: scale(0.9, 0.9) translate(0%, 5%); }30%  { transform: scale(1.3, 0.8) translate(0%, 10%); }50%  { transform: scale(0.8, 1.3) translate(0%, -10%); }70%  { transform: scale(1.1, 0.9) translate(0%, 5%); }100% { transform: scale(1.0, 1.0) translate(0%, 0%); }}</style>');
+document.querySelector("head").insertAdjacentHTML("afterbegin", '<style>#pome_angry_img {width: 100px; height: auto; bottom: 0px; left: 0px; position: fixed; z-index: 9999999; border-style: none; animation: pome_angry_move 10s linear 1s 12;} @keyframes pome_angry_move {0% { transform: scale(1.0, 1.0) translate(0%, 0%); }15%  { transform: scale(0.9, 0.9) translate(0%, 5%); }30%  { transform: scale(1.3, 0.8) translate(0%, 10%); }50%  { transform: scale(0.8, 1.3) translate(0%, -10%); }70%  { transform: scale(1.1, 0.9) translate(0%, 5%); }100% { transform: scale(1.0, 1.0) translate(0%, 0%); }}</style>');
 
 //HTML音声タグ
 const pome_work_alarm_tag = `<audio src="${pome_work_alarm_url}" id="pome_work_alarm"></audio>`;
@@ -31,7 +28,8 @@ const finish_alarm_tag = `<audio src="${finish_alarm_url}" id="finish_alarm"></a
 
 
 //dialogの共通css
-const dialog_style = `width: 300px; height: 100px; border: none; border-radius: 10px; background-size: 100% 100%;`
+const dialog_style = `width: 300px; height: 110px; border: none; border-radius: 10px; background-size: 100% 100%; position: fixed; inset: 0;
+margin:auto;`
 const dialog_text_style = `"color: black; text-align: center; font-size: 30px; font-weight: bold; padding-top: 10px; color: #330000; text-shadow: 1.5px 1.5px 0 #778899; -webkit-text-stroke: 1px #888; text-stroke: 1px #888;"`
 const ok_btn_style = `"color: black; padding: 3px 10px; margin-top: 10px; margin-left: 130px; background-color: #FFFFDD; border-color: #FFFFBB; border-radius: 10px;"`
 
